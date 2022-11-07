55eb3c081e18f879a8025a5740750caa78dc21ec
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2022-11-07 16:28:22 +0100

    for whatsapp api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import {ClientsService} from "./services/clients.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ManageClientsComponent } from './manage-clients/manage-clients.component';
+import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ManageClientsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
